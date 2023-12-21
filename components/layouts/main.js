2ba924de1b337ff5f447ff27c43b6cdcd1fb6dc9
Landon Nguyen <landonnguyen77@gmail.com>
2023-12-21 10:04:27 -0600

    initial commit

---
+++ components/layouts/main.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import {Box} from '@chakra-ui/react'
+const Main = ({children, router}) => {
+    return (
+        <Box as="main" pb={8}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Landon Nguyen - Homepage</title>
+            </Head>
+        </Box>
+    )
+}
